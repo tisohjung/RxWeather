274eedb5e403aea36fc995861269b9d1d5b2c7c9
Minho Yi <tisohjung@gmail.com>
2021-05-29 17:50:52 +0900

    setup storyboards

--- RxWeather/ViewControllers/ViewController.swift
+++ RxWeather/ViewControllers/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tfCityName: UITextField!
+    @IBOutlet weak var lbTemperature: UILabel!
+    @IBOutlet weak var lbHumidity: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
-
